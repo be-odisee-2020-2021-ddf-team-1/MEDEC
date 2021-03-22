2bbc9fbf4bc0c64fe6c549fb8a79732324f83a42
adrientaneko <55691233+adrientaneko@users.noreply.github.com>
2021-03-22 10:38:10 +0100

    Aanmaken van spring boot project

---
+++ src/main/java/be/odisee/medec/MedecApplication.java
@@ -0,0 +1,13 @@
+package be.odisee.medec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/odisee/medec/MedecApplicationTests.java
@@ -0,0 +1,13 @@
+package be.odisee.medec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
